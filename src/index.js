12e8b721ca17ead18483a8299915c8698e9ae5a4
alan.araujo <alan.araujo@softplan.com.br>
2018-11-23 16:27:17 -0200

    fix eslint with prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { IntlProvider } from "react-intl";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import * as serviceWorker from "./serviceWorker";
+import { initIntl, intlProviderProps } from "./config/intl";
+
+import App from "./App";
+
+import "./index.css";
+
+const renderApp = () => {
+  ReactDOM.render(
+    <IntlProvider {...intlProviderProps}>
+      <App />
+    </IntlProvider>,
+    document.getElementById("root")
+  );
+  serviceWorker.unregister();
+};
+
+initIntl().then(renderApp);
